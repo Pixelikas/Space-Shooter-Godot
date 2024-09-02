7eb77f0c4f510d8c62248c205dbfb306aac05df3
Lucas Naspolini <67108278+Pixelikas@users.noreply.github.com>
2024-09-01 23:55:09 -0300

    Add files via upload

--- Scripts/Enemy1.gd
+++ Scripts/Enemy1.gd
@@ -4,3 +4,9 @@ var speed = 50
 
 func _process(delta):
 	global_position.x -= speed * delta
+
+
+func _on_Hitbox_area_entered(area):
+	if area.is_in_group("Enemy_damager"):
+		area.get_parent().queue_free()
+		queue_free()
